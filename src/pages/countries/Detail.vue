246485f784b516415fa622cdfa9ce4c088ce6a9a
mauricio <mauricio@pricode.io>
2022-10-23 09:05:03 -0500

    styleguide package installation

---
+++ src/pages/countries/Detail.vue
@@ -0,0 +1,4 @@
+<template>
+    Detail countrie
+</template>
+
---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Countries  from '@/pages/countries/List.vue';
+import Detail from '@/pages/countries/Detail.vue';
+
+const routes = [
+    { path: '/', component: Countries },
+    { path: '/detail', component: Detail },
+];
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes,
+});
+
+export default router;
